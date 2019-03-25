bf1510d56ec012a39e8a25838ef55233beb8d05e
Nicolas Metz <NicolasMetz@Nicolass-MBP.fritz.box>
2019-03-25 21:45:18 +0100

    2/10

--- workload/3/main.js
+++ workload/3/main.js
@@ -37,3 +37,9 @@ console.log('    Remove whitespace   '.trim());
 console.log(Math.floor(Math.random() * 100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
+
+
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
