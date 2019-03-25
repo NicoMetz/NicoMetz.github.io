9b15ff7fd3856601d5a3ad37118b8c4065ade272
Nicolas Metz <NicolasMetz@Nicolass-MBP.fritz.box>
2019-03-25 22:11:57 +0100

    9/10

--- workload/3/main.js
+++ workload/3/main.js
@@ -85,3 +85,10 @@ let myCity = 'San Fran';
 console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
 
 
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+
+newVariable = 1;
+
+console.log(typeof newVariable);
