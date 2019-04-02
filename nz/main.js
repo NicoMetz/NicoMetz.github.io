3dc4805ed1c42f486e0969dd93ab95aebb9ab4fe
Nicolas Metz <NicolasMetz@Nicolass-MBP.fritz.box>
2019-04-03 01:27:20 +0200

    Beautify

--- nz/main.js
+++ nz/main.js
@@ -1,18 +1,16 @@
-
-
 const div = document.getElementById("map");
 const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const titel = div.getAttribute("data-title");
 
-console.log("breite:",breite,"länge:",laenge,"Titel:",titel);
+console.log("breite:", breite, "länge:", laenge, "Titel:", titel);
 let karte = L.map("map");
 karte.setView(
-  [breite,laenge],10
+  [breite, laenge], 10
 );
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 let pin = L.marker(
-  [breite,laenge]
+  [breite, laenge]
 ).addTo(karte);
 pin.bindPopup(titel).openPopup();
