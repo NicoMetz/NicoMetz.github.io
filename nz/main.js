092812e114904ec72446960f11a6bce0553ea4f2
Nicolas Metz <NicolasMetz@Nicolass-MBP.fritz.box>
2019-04-03 01:17:15 +0200

    console für Karte

--- nz/main.js
+++ nz/main.js
@@ -4,3 +4,7 @@ const div = document.getElementById("map");
 const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const titel = div.getAttribute("data-title");
+
+console.log("breite:",breite,"länge:",laenge,"Titel:",titel);
+let karte = L.map("map");
+
