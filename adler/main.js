348ce2aff2146eac6f9f0c8b24874e3875291644
Nicolas Metz <NicolasMetz@ydWLI-E1-55-244.uibk.ac.at>
2019-04-03 16:29:15 +0200

    Update

--- adler/main.js
+++ adler/main.js
@@ -16,22 +16,20 @@ karte.setView(
   [breite, laenge], 13
 );
 
-//Openstreetmap einbinden
 
-//L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-//subdomains: ["a","b","c"],
-//attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-//}).addTo(karte);
 const kartenLayer = {
-osm: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png",{
+  osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    subdomains: ["a","b","c"],
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+
+geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png",{
   subdomains:["maps","maps1","maps2","maps3", "maps4"],
   attribution: 'Datenquelle: <a href:="https://www.basemap.at"> basemap.at</a>'
-})
-
-
+}),
 };
 
-).addTo(karte);
+kartenLayer.geolandbasemap.addTo(karte);
 
 //Positionsmarker Hinzufügen
 let pin = L.marker(
