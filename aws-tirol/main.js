e66c355f87372d2706d33f531e38a7099a94069c
Nicolas Metz <NicolasMetz@ydWLI-E1-55-101.uibk.ac.at>
2019-05-08 18:55:59 +0200

    LWD

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -71,7 +71,7 @@ kartenLayer.bmaphidpi.addTo(karte);
 
 //Auswahlmenü hinzufügen
 
-L.control.layers({
+const layerControl = L.control.layers({
     "Geoland Basemap": kartenLayer.geolandbasemap,
     "Geoland Basemap Grau": kartenLayer.bmapgrau,
     "OpenStreetMap": kartenLayer.osm,
@@ -84,16 +84,91 @@ L.control.layers({
 }).addTo(karte)
 
 karte.setView(
-    [47.267222, 11.392778],15);
+    [47.267222, 11.392778], 15);
 
- //   console.log(AWS)
-const awsTirol = L.featureGroup();
-    L.geoJson(AWS)
-    .bindPopup(function(layer){
-        console.log("Layer",layer);
-    return `Temperatur: ${layer.feature.properties.LT} °C<br>
-    Datum: ${layer.feature.properties.date}`;
-})
-    .addTo(awsTirol);
+async function loadStations() {
+    const response = await fetch("https://aws.openweb.cc/stations");
+    const stations = await response.json();
+    const awsTirol = L.featureGroup();
+    L.geoJson(stations)
+        .bindPopup(function (layer) {
+            //  console.log("Layer",layer);
+            const date = new Date(layer.feature.properties.date);
+            console.log(`Datum: `, date);
+            return `<h4>${layer.feature.properties.name}</h4>
+    Temperatur: ${layer.feature.properties.LT} °C<br>
+    HS: ${layer.feature.properties.HS} <br>
+    HSD24: ${layer.feature.properties.HSD24} <br>
+    HSD48: ${layer.feature.properties.HSD48} <br>
+    HSD72: ${layer.feature.properties.HSD72} <br>
+    Windgeschwindigkeit (km/h): 
+    ${layer.feature.properties.WG ? layer.feature.properties.WG : `Keine Daten`}<br>
+    Höhe: ${layer.feature.geometry.coordinates[2]} <br>
+    Datum: ${date.toLocaleDateString("de-AT")} 
+    ${date.toLocaleTimeString("de-AT")}
+    <hr>
+    <footer>Land Tirol - <a>"href=https://data.tirol.gv.at"> data.tirol.gv.at</a></footer>`;
+
+        })
+        .addTo(awsTirol);
     awsTirol.addTo(karte);
     karte.fitBounds(awsTirol.getBounds());
+    layerControl.addOverlay(awsTirol, "Wetterstationen Tirol");
+
+    const windLayer = L.featureGroup();
+    L.geoJson(stations, {
+        pointToLayer: function (feature, latlng) {
+            if (feature.properties.WR) {
+                let color = `black`;
+                if (feature.properties.WG > 20) {
+                    color = `red`;
+                }
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        html: `<i style="color:${color}; transform: rotate(${feature.properties.WR}deg)" class= "fas fa-arrow-circle-up fa-2x"></i>`
+                    })
+                })
+            }
+        }
+    }).addTo(windLayer);
+    layerControl.addOverlay(windLayer, "Windrichtung");
+    windLayer.addTo(karte);
+
+    const temperaturLayer = L.featureGroup();
+    const farbPalette = [
+        [0, "blue"],
+        [1, "yellow"],
+        [5, "orange"],
+        [10, "red"],
+    ];
+
+    L.geoJson(stations, {
+        pointToLayer: function (feature, latlng) {
+            if (feature.properties.LT) {
+                let color = `red`
+                for (let i = 0; i < farbPalette.length; i++) {
+                    console.log(farbPalette[i], feature.properties.LT);
+                    if (feature.properties.LT < farbPalette[i][0]) {
+                        color = farbPalette[i][1];
+                        break;
+                    }
+                }
+                //let color = `blue`;
+               // if (feature.properties.LT > 0) {
+               //     color = `red`;
+               // }
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        html: `<div class="temperaturLabel" style= "background-color:${color}"> ${feature.properties.LT}</div>`
+                    })
+                })
+            }
+        }
+    }).addTo(temperaturLayer);
+    layerControl.addOverlay(temperaturLayer, "Temperatur");
+    temperaturLayer.addTo(karte);
+}
+//   console.log(AWS)
+
+
+loadStations();
